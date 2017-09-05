5496cc1827c40787f71f9ca1d9b1234d1bbab290
kksTest <kskim1458@gmail.com>
2017-09-05 14:18:10 +0900

    test 브랜치 작업 2 추가

--- HelloProject/src/HelloTest.java
+++ HelloProject/src/HelloTest.java
@@ -3,5 +3,6 @@ public class HelloTest {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("test 브랜치 작업 1");
+		System.out.println("test 브랜치 작업 2");
 	}
 }
